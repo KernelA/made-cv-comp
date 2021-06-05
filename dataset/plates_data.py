bb3c969469645572007a7bf527eb4156b44d2b45
KernelA <17554646+KernelA@users.noreply.github.com>
2021-06-05 19:01:19 +0300

    Small fix

--- dataset/plates_data.py
+++ dataset/plates_data.py
@@ -1,12 +1,10 @@
 import os
-import math
 
 import cv2
 from torch.utils import data
 import numpy as np
 from matplotlib.path import Path
 import torch
-from PIL import Image
 
 
 class DetectionDataset(data.Dataset):
@@ -14,56 +12,6 @@ class DetectionDataset(data.Dataset):
         self.marks = marks
         self.img_folder = img_folder
         self.transforms = transforms
-        self._plates_info = []
-        self._prepare_data()
-
-    def _prepare_data(self):
-        for plate_info in self.marks:
-            img_path = os.path.join(self.img_folder, plate_info["file"])
-
-            with Image.open(img_path) as image:
-                height, width = image.height, image.width
-
-            box_coords = plate_info['nums']
-            boxes = []
-            labels = torch.ones(len(box_coords), dtype=torch.long)
-            masks = []
-
-            for box in box_coords:
-                points = np.array(box['box'])
-                x0, y0 = np.min(points[:, 0]), np.min(points[:, 1])
-                x2, y2 = np.max(points[:, 0]), np.max(points[:, 1])
-
-                x0 = np.clip(x0, 0, width - 1)
-                x2 = np.clip(x2, 0, width - 1)
-
-                y0 = np.clip(y0, 0, height - 1)
-                y2 = np.clip(y2, 0, height - 1)
-
-                boxes.append([x0, y0, x2, y2])
-
-                # Здесь мы наши 4 точки превращаем в маску
-                # Это нужно, чтобы кроме bounding box предсказывать и, соответственно, маску :)
-                nx, ny = width, height
-                poly_verts = points
-                x, y = np.meshgrid(np.arange(nx), np.arange(ny))
-                x, y = x.flatten(), y.flatten()
-                points = np.vstack((x, y)).T
-                path = Path(poly_verts)
-                grid = path.contains_points(points)
-                grid = grid.reshape((ny, nx)).astype(int)
-                masks.append(grid)
-
-            boxes = torch.tensor(boxes)
-            masks = torch.tensor(masks)
-
-            target = {
-                'boxes': boxes,
-                'labels': labels,
-                'masks': masks,
-            }
-
-            self._plates_info.append(target)
 
     def __getitem__(self, index):
         item = self.marks[index]
@@ -71,10 +19,51 @@ class DetectionDataset(data.Dataset):
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+        height, width = img.shape[:2]
+
+        box_coords = item['nums']
+        boxes = []
+        labels = torch.ones(len(box_coords), dtype=torch.long)
+        masks = []
+
+        for box in box_coords:
+            points = np.array(box['box'])
+            x0, y0 = np.min(points[:, 0]), np.min(points[:, 1])
+            x2, y2 = np.max(points[:, 0]), np.max(points[:, 1])
+
+            x0 = np.clip(x0, 0, width - 1)
+            x2 = np.clip(x2, 0, width - 1)
+
+            y0 = np.clip(y0, 0, height - 1)
+            y2 = np.clip(y2, 0, height - 1)
+
+            boxes.append([x0, y0, x2, y2])
+
+            # Здесь мы наши 4 точки превращаем в маску
+            # Это нужно, чтобы кроме bounding box предсказывать и, соответственно, маску :)
+            nx, ny = width, height
+            poly_verts = points
+            x, y = np.meshgrid(np.arange(nx), np.arange(ny))
+            x, y = x.flatten(), y.flatten()
+            points = np.vstack((x, y)).T
+            path = Path(poly_verts)
+            grid = path.contains_points(points)
+            grid = grid.reshape((ny, nx)).astype(int)
+            masks.append(grid)
+
+        boxes = torch.tensor(boxes)
+        masks = torch.tensor(masks)
+
+        target = {
+            'boxes': boxes,
+            'labels': labels,
+            'masks': masks,
+        }
+
         if self.transforms is not None:
             img = self.transforms(img)
 
-        return img, self._plates_info[index]
+        return img, target
 
     def __len__(self):
         return len(self.marks)
